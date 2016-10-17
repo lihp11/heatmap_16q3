f68605b7320339a8a14bcf939dbdb21f40e07a72
Frankie Lee <lihuiping19930203@163.com>
2016-10-17 09:29:53 +0800

    modify_mz
    modify mingzhu's program

--- headmap_mz.m
+++ headmap_mz.m
@@ -1,68 +1,170 @@
-%step 1 ********************************导出数据
-%从天津数据库中获得的文件gz_nonring_20150607_20.txt转成Data
-%Data数据说明（列）：1 行程分组按时间升序的编号（1代表起点）；
-%                    2 行程分组按时间降序的编号（1代表终点）；
-%                    3 去除“tid_”的行程编号
-%                    4 导航用户 ID
-%                    5 数据采集时间
-%                    6 匹配后的经度
-%                    7 匹配后的纬度
-%Trip数据说明（列）：1 行程分组采集数量统计
-%                    2 行程编号（全数字）
-%                    3 导航用户ID
-%                    4 导航起点时间
-%                    5 起点经度
-%                    6 起点纬度
-%                    7 导航终点时间
-%                    8 终点经度
-%                    9 终点纬度
-%step 4 中增加      10 起点x的编号
-%step 4 中增加      11 起点y的编号
-%step 4 中增加      12 终点x的编号
-%step 4 中增加      13 终点y的编号
+%% export data
 
-Data= dlmread('gz_nonring_20150607_20.txt'); %14251144 records
+Data= dlmread('gz_nonring_20150607_20.txt');
+	 % array:(pure od data) rnum=many,c1=nth timestamp from start (1 for origin)
+	 % c2=nth timestamp from end (1 for destin),c3=trajectory_id,c4=user_id
+	 %c5=time(eg 201501010930),c6=long,c7=lat
 Trip= [Data(1:2:end,2:end),Data(2:2:end,[5,6,7])];%7125572 records
+	% array:rnum=num of trips,c1=num of gps point in for one trip
+	% c2=trajectory_id,c3= user_id,c4=start_time(eg YYYYMMDDHHMMSS),c5=start long
+	% c6=start lat,c7=end_time(eg 201501010930),c8=end long,c9=end lat
 
-%step 2 ********************************数据清理
-Trip(Trip(:,1)<11,:)= [];   %清除采集数量少于10的行程, 3761453 records remain
-Trip(Trip(:,5)<=0 | Trip(:,6)<=0 | Trip(:,8)<=0 | Trip(:,9)<=0,:)= []; %清除经纬度无效数据， 3495195 records remain
-Trip(TimeCalculate(Trip(:,4),Trip(:,7))<60,:)= []; %清除导航时间小于1分钟的数据条， 3474394 records remain
+%% del error data
 
-%step 3 ********************************建立经纬度网格
-maxx= max([Trip(:,5);Trip(:,8)])+0.01;
-minx= min([Trip(:,5);Trip(:,8)])-0.01;
-maxy= max([Trip(:,6);Trip(:,9)])+0.01;
-miny= min([Trip(:,6);Trip(:,9)])-0.01;
-zonegap= 0.5;                        % 单位为km
-xgap= zonegap/85.37295;              % 设定经度间距,单位千米
-ygap= zonegap/111.3193;              % 设定纬度间距单位千米
-xnum= ceil((maxx-minx)/xgap);        % X方向小区的个数
-ynum= ceil((maxy-miny)/ygap);        % Y方向小区的个数
-[x,y]= meshgrid(minx+xgap/2:xgap:minx+xnum*xgap-xgap/2,...
-    miny+ygap/2:ygap:miny+ynum*ygap-ygap/2);          %  得到各小区的中心点
+Trip(Trip(:,1)<11,:)= [];
+Trip(Trip(:,5)<=0 | Trip(:,6)<=0 | Trip(:,8)<=0 | Trip(:,9)<=0,:)= [];
+Trip(TimeCalculate(Trip(:,4),Trip(:,7))<60,:)= [];
 
-%step 4 ********************************Trip中数据在X,Y的对应 增加4列
-Trip(:,10)= ceil((Trip(:,5)-minx)/xgap);                   %起点横坐标编号
-Trip(:,11)= ceil((Trip(:,6)-miny)/ygap);                   %起点纵坐标编号
-Trip(:,12)= ceil((Trip(:,8)-minx)/xgap);                   %终点横坐标编号
-Trip(:,13)= ceil((Trip(:,9)-miny)/ygap);                   %终点纵坐标编号
+%% setup mesh grid
+clc,clear,close all
+load('od_rawdata');
 
-%step 5 ********************************生成热力图矩阵
+% change here!!!
+zonegap= 0.5;        % single = cube length(km)
+red_alert = 1000;     % single = color upper limit(all num of cars exceeding it will be treated the same)
+
+maxx= max([Trip(:,5);Trip(:,8)])+0.01;	%single = max long in total data
+minx= min([Trip(:,5);Trip(:,8)])-0.01;	%single = min long in total data
+maxy= max([Trip(:,6);Trip(:,9)])+0.01;	%single = max lat in total data
+miny= min([Trip(:,6);Trip(:,9)])-0.01;	%single = min lat in total data
+xgap= zonegap/85.37295;              % single = long gap for cubelength
+ygap= zonegap/111.3193;              % single = lat gap for cubelength
+xnum= ceil((maxx-minx)/xgap);        % single = num of cubes in x
+ynum= ceil((maxy-miny)/ygap);        % single = num of cubes in y
+[x,y]= meshgrid(minx+xgap/2:xgap:minx+xnum*xgap-xgap/2,miny+ygap/2:ygap:miny+ynum*ygap-ygap/2);
+	% x or y:array,rnum=cubes in x,cnum=cube in y,content=long or lat
+
+%% calc od data's cube index
+Trip(:,10)= ceil((Trip(:,5)-minx)/xgap);
+Trip(:,11)= ceil((Trip(:,6)-miny)/ygap);
+Trip(:,12)= ceil((Trip(:,8)-minx)/xgap);
+Trip(:,13)= ceil((Trip(:,9)-miny)/ygap);
+	% array:rnum=num of trips,c1=num of gps point in for one trip
+	% c2=trajectory_id,c3= user_id,c4=start_time(eg 201501010930),c5=start long
+	% c6=start lat,c7=end_time(eg 201501010930),c8=end long,c9=end lat
+	% c10=x_index of startcube,c11=y_index of startcube
+	% c12=x_index of endcube,c13=y_index of endcube
+
+%% calc od matrix
+
+clear('Demand_O','Demand_D');
 Demand_O= zeros(ynum,xnum);
 Demand_D= zeros(ynum,xnum);
-for xi= 1:xnum
-    for yi= 1:ynum
-        xi
-        yi
-        Demand_O(yi,xi)= sum(Trip(:,10)==xi & Trip(:,11)==yi);
-        Demand_D(yi,xi)= sum(Trip(:,12)==xi & Trip(:,13)==yi);
-    end
+
+for nth_rec = 1:length(Trip)
+	if mod(nth_rec,10000)==0
+		disp(['Calcling ',num2str(nth_rec),' Trips!'])
+	end
+	Demand_O(Trip(nth_rec,10),Trip(nth_rec,11)) = Demand_O(Trip(nth_rec,10),Trip(nth_rec,11))+1;
+	% array:rnum= num of cubes in x,cnum=num of cubes in y,content=num of origin point at this cube
+	Demand_D(Trip(nth_rec,12),Trip(nth_rec,13)) = Demand_D(Trip(nth_rec,12),Trip(nth_rec,13))+1;
+	% array:rnum= num of cubes in x,cnum=num of cubes in y,content=num of destin point at this cube
 end
 
-%step 6 ********************************简单画出热力图
-%起点热力图
+%% plot heatmap
 figure
-    surf(x,y,Demand_O,'FaceColor','interp',...
-            'EdgeColor','none',...
-            'FaceLighting','phong');
+    surf(x,y,Demand_O,'FaceColor','interp','EdgeColor','none','FaceLighting','phong');
+colormap('parula')
+
+index_rowstart = ceil(600/1804*size(Demand_O,1));
+	% single = index of beijing x starting for Demand mat
+index_rowend = ceil(1000/1804*size(Demand_O,1));
+	% single = index of beijing x ending for Demand mat
+index_colstart = ceil(400/1793*size(Demand_O,2));
+	% single = index of beijing y starting for Demand mat
+index_colend = ceil(700/1793*size(Demand_O,2));
+	% single = index of beijing y ending for Demand mat
+
+close all;
+figure('Name','origin_headtmap');
+imagesc(Demand_O(index_rowstart:index_rowend,index_colstart:index_colend));
+colormap('jet');
+caxis([0,red_alert])
+colorbar;
+figure('Name','dest_headtmap');
+imagesc(Demand_D(index_rowstart:index_rowend,index_colstart:index_colend));
+colormap('jet');
+caxis([0,red_alert])
+colorbar;
+
+%% gps od time statistic
+otime= rem(fix(Trip(:,4)/1e+4),100)+rem(fix(Trip(:,4)/1e+2),100)/60;
+	% vector:num=num of trips,content=start_hours in a day 
+dtime= rem(fix(Trip(:,7)/1e+4),100)+rem(fix(Trip(:,7)/1e+2),100)/60;
+	% vector:num=num of trips,content=end_hours in a day 
+
+figure('name','otime_distribution')
+histogram(otime,0.5:23.5,'FaceColor',[0,0.5,0.5])
+title('otime_distribution','fontname','等线','fontsize',15,'fontweight','bold','interpreter','none')
+set(gca,'xlim',[0,24],'xtick',0:24)
+xlabel('hour of day','fontname','等线','fontsize',12,'fontweight','bold')
+ylabel('counts','fontname','等线','fontsize',12,'fontweight','bold')
+
+figure('name','dtime_distribution')
+histogram(dtime,0.5:23.5,'FaceColor',[0,0.5,0.5])
+title('dtime_distribution','fontname','等线','fontsize',15,'fontweight','bold','interpreter','none')
+set(gca,'xlim',[0,24],'xtick',0:24)
+xlabel('hour of day','fontname','等线','fontsize',12,'fontweight','bold')
+ylabel('counts','fontname','等线','fontsize',12,'fontweight','bold')
+
+%% gps continue time statistic
+continue_time = (dtime-otime)*60;
+figure('name','continue_time_distribution')
+histogram(continue_time,0:10:200,'FaceColor',[0,0.5,0.5])
+title('continue_time_distribution','fontname','等线','fontsize',15,'fontweight','bold','interpreter','none')
+set(gca,'xlim',[0,200])
+xlabel('trip time(min)','fontname','等线','fontsize',12,'fontweight','bold')
+ylabel('counts','fontname','等线','fontsize',12,'fontweight','bold')
+
+
+%% get desireline graph
+num_cube = xnum*ynum;	% single=num of total cubes
+x_center = minx+xgap/2:xgap:minx+xnum*xgap-xgap/2;
+y_center = miny+ygap/2:ygap:miny+ynum*ygap-ygap/2;
+% up2:vector:num=num of cubes in x or y,content=long or lat
+od_table = sparse(num_cube,num_cube);
+% array:rnum=cnum=num of total cubes,content=num of traffic
+
+nth_ocube = ynum*(Trip(:,10)-1)+Trip(:,11);
+nth_dcube = ynum*(Trip(:,12)-1)+Trip(:,13);
+% up2:vector:num=num of total cubes,content=long or lat
+
+% array:rnum=cnum=num_cube
+
+for nth_trip = 1:length(Trip)
+	if mod(nth_trip,10000)==0
+		disp(['Calcling ',num2str(nth_trip),' Trips!'])
+	end
+	od_table(nth_ocube(nth_trip),nth_dcube(nth_trip)) = od_table(nth_ocube(nth_trip),nth_dcube(nth_trip))+1;
+end
+
+od_total = tril(od_table)+tril(od_table')-diag(diag(od_table));
+% array:rnum=cnum=num of total cubes,content=num of interact traffic
+[od_row,od_col] = find(od_total>100);
+% od_row:vector,num=row index (nth_cube) satisfy interact traffic>100
+% od_col:vector,num=col index (nth_cube) satisfy interact traffic>100
+od_max = max(max(od_total));	%single = max od
+
+figure('Name','desireline');
+for nth_line = 1:length(od_row)
+	if mod(nth_line,1000)==0
+		disp(['Calcling ',num2str(nth_line),' Trips!'])
+	end
+	nth_start_xcube = ceil(od_row(nth_line)/ynum);
+	% single = nth cube in x for the start point
+	nth_start_ycube = mod(od_row(nth_line),ynum);
+	nth_end_xcube = ceil(od_col(nth_line)/ynum);
+	nth_end_ycube = mod(od_col(nth_line),ynum);
+
+	plot([x_center(nth_start_xcube),x_center(nth_end_xcube)],...
+		 [y_center(nth_start_ycube),y_center(nth_end_ycube)],...
+		 'linewidth',20*od_total(od_row(nth_line),od_col(nth_line))/od_max,...
+         'color','r');
+	hold on;
+end
+axis equal;
+title('desireline','fontname','等线','fontsize',15,'fontweight','bold','interpreter','none')
+xlabel('longtitude','fontname','等线','fontsize',12,'fontweight','bold')
+ylabel('latitude','fontname','等线','fontsize',12,'fontweight','bold')
+
+
--- 需求热力图和期望线new.m
+++ 需求热力图和期望线new.m
@@ -1,179 +1,150 @@
-%%%%%%%%%%%%%%%%%%%%%导出数据
-TripOD=dlmread('TripOD_04.txt');%13229614个行程
-%检查行程距离，进一步清晰数据（清理掉大于100km的行程）
-noreason1=find(TripOD(:,9)>100);%15791条记录
-TripOD(noreason1,:)=[];%13213823条记录
-%清理掉小于0.5km的记录
-noreason2=find(TripOD(:,9)<0.5);%83202条记录
-TripOD(noreason2,:)=[];%剩13130621条记录
-%选择时间段
-nowork1=find(TripOD(:,3)>=20150404000000 ...
-    &TripOD(:,3)<20150407000000);%清明假期1012545条记录
-nowork2=find(TripOD(:,3)>=20150411000000 ...
-    &TripOD(:,3)<20150413000000);%周末765789条记录
-nowork3=find(TripOD(:,3)>=20150418000000 ...
-    &TripOD(:,3)<20150420000000);%周末783450条记录
-nowork4=find(TripOD(:,3)>=20150425000000 ...
-    &TripOD(:,3)<20150427000000);%周末800833条记录
-work1=find(TripOD(:,3)>=20150401000000 ...
-    &TripOD(:,3)<20150404000000);%3天工作日1354363条记录
-work2=find(TripOD(:,3)>=20150407000000 ...
-    &TripOD(:,3)<20150411000000);%4个工作日1821598条记录
-work3=find(TripOD(:,3)>=20150413000000 ...
-    &TripOD(:,3)<20150418000000);%5个工作日2319125条记录
-work4=find(TripOD(:,3)>=20150420000000 ...
-    &TripOD(:,3)<20150425000000);%5个工作日2390584条记录
-work5=find(TripOD(:,3)>=20150427000000 ...
-    &TripOD(:,3)<20150500000000);%4个工作日1882342条记录
-nowork=[nowork1;nowork2;nowork3;nowork4];
-work=[work1;work2;work3;work4;work5];
-%工作日
-travelod_wk=[];
-travelod_wk=TripOD([work1;work2;work3;work4;work5],[4,5,7,8,3,6]);
-travelnode_wk=[travelod_wk(:,1:2);travelod_wk(:,(3:4))];
-%%%%%%%%%%%%%%%%%%%%%%%%%%%建立经纬度网格
-maxlongitude=max(travelnode_wk(:,1));
-minlongitude=min(travelnode_wk(:,1));
-maxlatitude=max(travelnode_wk(:,2));
-minlatitude=min(travelnode_wk(:,2));
-M,N]=size(travelnode_wk);
-maxy=maxlatitude+0.01;
-miny=minlatitude-0.01;
-maxx=maxlongitude+0.01;
-minx=minlongitude-0.01;
-zonegap=1; %单位为km
-longitudegap=zonegap/85.37295;    %设定经度间距,单位千米
-latitudegap=zonegap/111.3193;        %设定纬度间距，单位千米
-xnumber=ceil((maxx-minx)/longitudegap);
-ynumber=ceil((maxy-miny)/latitudegap);
-[x3,y3]=meshgrid(minx+longitudegap/2:longitudegap:minx+xnumber*longitudegap-longitudegap/2,...
-    miny+latitudegap/2:latitudegap:miny+ynumber*latitudegap-latitudegap/2);%这样画出来的图中第一个矩阵块少半块
-z3=ones(size(x3,1),size(x3,2));
+%step 1 ********************************导出数据
+%从天津数据库中获得的文件gz_nonring_20150607_20.txt转成Data
+%Data数据说明（列）：1 行程分组按时间升序的编号（1代表起点）；
+%                    2 行程分组按时间降序的编号（1代表终点）；
+%                    3 去除“tid_”的行程编号
+%                    4 导航用户ID
+%                    5 数据采集时间
+%                    6 匹配后的经度
+%                    7 匹配后的纬度
+%Trip数据说明（列）：1 行程分组采集数量统计
+%                    2 行程编号（全数字）
+%                    3 导航用户ID
+%                    4 导航起点时间
+%                    5 起点经度
+%                    6 起点纬度
+%                    7 导航终点时间
+%                    8 终点经度
+%                    9 终点纬度
+%step 4 中增加      10 起点x的编号（按照xy矩阵）
+%step 4 中增加      11 起点y的编号（按照xy矩阵）
+%step 4 中增加      12 终点x的编号（按照xy矩阵）
+%step 4 中增加      13 终点y的编号（按照xy矩阵）
+%step 8 中增加      14 OD矩阵行的编号（起点）（按照xy矩阵）
+%step 8 中增加      15 OD矩阵列的编号（终点）（按照xy矩阵）
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%准备travelodtime矩阵
-travelodtime(:,1:6)=travelod_wk(:,[1,2,5,3,4,6]);
-travelodtime(:,[3,6])=fix(rem(travelodtime(:,[3,6]),1e+6)/100);            %将时间格式由20141001193521改成时分式1935
-%travelodtime矩阵的列的安排：1 O_longtitude  2 O_latitude  3 O_time  4 D_longtitude
-%5 D_latitude  6 D_time 格式为2359，表示23点59分 7 O_longtidudenum  8 O_latitudenum  9 O_timenum
-%10 D_longtitudenum  11 D_latitudenum  12 D_timenum  13 O_rownum  14
-%D_columnnum
-maxtime=2359;  %设定最大时间点为23点59分
-mintime=1;     %设定最小时间点为00点01分
-maxz=maxtime+1;
-minz=mintime-1;
-maxz_c=fix(maxz/100)*60+rem(maxz,100); %转化时间格式为分钟
-minz_c=fix(minz/100)*60+rem(minz,100); %转化时间格式为分钟
-timegap=60;    %设定时间间隔为60分钟
-znumber=ceil((maxz_c-minz_c)/timegap);
-travelodtime(:,7)=ceil((travelodtime(:,1)-minx)/longitudegap);                   %travelodtime 中 7 起点横坐标编号
-travelodtime(:,8)=ceil((travelodtime(:,2)-miny)/latitudegap);                    %travelodtime 中 8 起点纵坐标编号
-travelodtime(:,9)=ceil(((fix(travelodtime(:,3)/100)*60+rem(travelodtime(:,3),100))-minz_c)/timegap);           %travelodtime 中 9 起点Z坐标
-travelodtime(travelodtime(:,9)==0,9)=1;
-travelodtime(:,10)=ceil((travelodtime(:,4)-minx)/longitudegap);                  %travelodtime 中10 终点横坐标编号
-travelodtime(:,11)=ceil((travelodtime(:,5)-miny)/latitudegap);                   %travelodtime 中11 终点纵坐标编号
-travelodtime(:,12)=ceil(((fix(travelodtime(:,6)/100)*60+rem(travelodtime(:,6),100))-minz_c)/timegap);          %travelodtime 中12 终点Z坐标
-travelodtime(travelodtime(:,12)==0,12)=1;
-travelodtime(:,13)=ynumber*(travelodtime(:,7)-1)+travelodtime(:,8)+(travelodtime(:,9)-1)*xnumber*ynumber;                %travelodtime 中 13 起点在三维期望线（OD）矩阵中对应的行的位置
-travelodtime(:,14)=ynumber*(travelodtime(:,10)-1)+travelodtime(:,11)+(travelodtime(:,12)-1)*xnumber*ynumber;             %travelodtime 中14 终点在三维期望线（OD）矩阵中对应的列的位置
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%建立odddemand思维矩阵
-%要在trvalodtime中找到每个O点和D点的在oddemand矩阵中的对应关系
-%画每个时间段内的demand分布情况（图层）依据travelodtime导出四维矩阵oddemad(x,y,z,type),type=1表示O+D,type=2表示O
-%type=3表示D
-oddemand=zeros(xnumber,ynumber,znumber,3);
-numod=size(travelodtime,1);
-for i=1:numod
-    oxi=travelodtime(i,7);
-    oyi=travelodtime(i,8);
-    ozi=travelodtime(i,9);
-    if ozi==0
-        ozi=1;
-    end
-    dxi=travelodtime(i,10);
-    dyi=travelodtime(i,11);
-    dzi=travelodtime(i,12);
-    if dzi==0
-        dzi=1;
-    end    
-    oddemand(oxi,oyi,ozi,2)=oddemand(oxi,oyi,ozi,2)+1;
-    oddemand(dxi,dyi,dzi,3)=oddemand(dxi,dyi,dzi,3)+1;
-end
-for i=1:znumber
-    oddemand(:,:,i,1)=oddemand(:,:,i,2)+oddemand(:,:,i,3);
-end
-%完成oddemand四维矩阵
+Data= dlmread('gz_nonring_20150607_20.txt'); %14251144 records
+Trip= [Data(1:2:end,2:end),Data(2:2:end,[5,6,7])];%7125572 records
+
+%step 2 ********************************数据清理
+
+Trip(Trip(:,1)<11,:)= [];   %清除采集数量小于等于10的行程, 3761453 records remain
+Trip(Trip(:,5)<=0 | Trip(:,6)<=0 | Trip(:,8)<=0 | Trip(:,9)<=0,:)= []; %清除经纬度无效数据， 3495195 records remain
+Trip(TimeCalculate(Trip(:,4),Trip(:,7))<60,:)= []; %清除导航时间小于1分钟的数据条， 3474394 records remain
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%热力图
-type=1;
-C1_all=zeros(size(x3,1),size(x3,2));
-for  i=1:znumber
-    C1_all=C1_all+oddemand(:,:,i,type)';
+
+%step 3 ********************************建立经纬度网格
+
+maxx= max([Trip(:,5);Trip(:,8)])+0.01;
+minx= min([Trip(:,5);Trip(:,8)])-0.01;
+maxy= max([Trip(:,6);Trip(:,9)])+0.01;
+miny= min([Trip(:,6);Trip(:,9)])-0.01;
+zonegap= 1;                          % 单位为km
+xgap= zonegap/85.37295;              % 设定经度间距,单位千米
+ygap= zonegap/111.3193;              % 设定纬度间距,单位千米
+xnum= ceil((maxx-minx)/xgap);        % X方向小区的个数
+ynum= ceil((maxy-miny)/ygap);        % Y方向小区的个数
+[x,y]= meshgrid(minx+xgap/2:xgap:minx+xnum*xgap-xgap/2,...
+    miny+ygap/2:ygap:miny+ynum*ygap-ygap/2);               %得到各小区的中心点
+
+
+
+%step 4 ********************************Trip中数据在X,Y的对应 增加4列
+
+Trip(:,10)= ceil((Trip(:,5)-minx)/xgap);                   %起点横坐标编号
+Trip(:,11)= ceil((Trip(:,6)-miny)/ygap);                   %起点纵坐标编号
+Trip(:,12)= ceil((Trip(:,8)-minx)/xgap);                   %终点横坐标编号
+Trip(:,13)= ceil((Trip(:,9)-miny)/ygap);                   %终点纵坐标编号
+
+
+
+%step 5 ********************************生成热力图矩阵
+
+Demand_O= zeros(ynum,xnum);
+Demand_D= zeros(ynum,xnum);
+for nth_rec = 1:length(Trip)
+	if mod(nth_rec,10000)==0
+		disp(['Calcling ',num2str(nth_rec),' records!'])
+	end
+	Demand_O(Trip(nth_rec,10),Trip(nth_rec,11)) = Demand_O(Trip(nth_rec,10),Trip(nth_rec,11))+1;
+	Demand_D(Trip(nth_rec,12),Trip(nth_rec,13)) = Demand_O(Trip(nth_rec,12),Trip(nth_rec,13))+1;
 end
-type=2;
-C2_all=zeros(size(x3,1),size(x3,2));
-for  i=1:znumber
-    C2_all=C2_all+oddemand(:,:,i,type)';
-end
-type=3;
-C3_all=zeros(size(x3,1),size(x3,2));
-for  i=1:znumber
-    C3_all=C3_all+oddemand(:,:,i,type)';
-end
-%画出type=1的需求热点图
-CC=C1_all;
+
+
+
+%step 6 ********************************简单画出热力图
+
+%起点热力图
 figure
-    surf(x3,y3,z3*0,CC,'FaceColor','interp',...
+    surf(x,y,Demand_O,'FaceColor','interp',...
             'EdgeColor','none',...
             'FaceLighting','phong');
     axis xy;
     colormap(hot)
     colorbar
-    cmap=colormap;
-    definedmap=flip(cmap);
+    cmap= colormap;
+    definedmap= flip(cmap);
     colormap(definedmap);
     caxis([0,100000]);
     view(2)
     eval(['title(''type=',num2str(3),''')'])
-
     
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%画期望线，建立期望线矩阵desireline_two demension
-ds=xnumber*ynumber;
-desireline=zeros(ds,ds);
-travelod_wk(:,7)=ceil((travelod_wk(:,1)-minx)/longitudegap);                   %travelod 中 7 起点横坐标
-travelod_wk(:,8)=ceil((travelod_wk(:,2)-miny)/latitudegap);                    %travelod 中 8 起点纵坐标
-travelod_wk(:,9)=ceil((travelod_wk(:,3)-minx)/longitudegap);                   %travelod 中 9 终点横坐标
-travelod_wk(:,10)=ceil((travelod_wk(:,4)-miny)/latitudegap);                    %travelod 中 10 终点横坐标
-travelod_wk(:,11)=ynumber*(travelod_wk(:,7)-1)+travelod_wk(:,8);                     %travelod 中 11 起点在二维期望线（OD）矩阵中对应的行的位置
-travelod_wk(:,12)=ynumber*(travelod_wk(:,9)-1)+travelod_wk(:,10);                    %travelod 中12 终点在二维期望线（OD）矩阵中对应的列的位置
-for i=1:size(travelod_wk,1)
-    desireline(travelod_wk(i,11),travelod_wk(i,12))=desireline(travelod_wk(i,11),travelod_wk(i,12))+1;  %在期望线（OD）矩阵中写入相应的值
+    figure
+    surf(x,y,Demand_D,'FaceColor','interp',...
+            'EdgeColor','none',...
+            'FaceLighting','phong');
+
+
+
+%step 7 ********************************统计分析
+
+%导航起点和终点时间分布统计
+OTime= rem(fix(Trip(:,4)/1e+4),100)+rem(fix(Trip(:,4)/1e+2),100)/60; %时间单位：小时
+DTime= rem(fix(Trip(:,7)/1e+4),100)+rem(fix(Trip(:,7)/1e+2),100)/60; %时间单位：小时
+edges1 = [0:24];
+OTime_hist= histogram(OTime,edges1);
+figure
+DTime_hist= histogram(DTime,edges1);
+
+%导航时长分布统计
+ODTime= TimeCalculate(Trip(:,4),Trip(:,7))/60;           %时间单位：分钟
+edges2= [0:15:max(ODTime)];
+figure
+ODTime_hist= histogram(ODTime,edges2);
+
+
+
+%step 8 ********************************生成期望线图矩阵
+
+xynum= xnum*ynum;          %将所有的小区列为一行
+Trip(:,14)= ynum*(Trip(:,10)-1)+Trip(:,11);              %OD矩阵行编号
+Trip(:,15)= ynum*(Trip(:,12)-1)+Trip(:,13);              %OD矩阵列编号
+ODmatrix= sparse(xynum,xynum);
+for i= 1:size(Trip,1)
+    ODmatrix(Trip(i,14),Trip(i,15))= ODmatrix(Trip(i,14),Trip(i,15))+1;  %累加
 end
-noindex=find(desireline>10);  %选择所画的期望线的数值范围
-maxdesireline=max(max(desireline)); %找出期望线的最大值，在画期望线粗细的时候会用到
-figure                          %画出热力图
-surf(x3,y3,z3*0,C_all,'FaceColor','interp',...
-        'EdgeColor','none',...
-        'FaceLighting','phong');
-    
-colormap(definedmap);
-colorbar
-caxis([100,100000]);
-
-
-hold on   %添加二维望线
-for j=1:length(noindex)         %画期二维望线
-    i=noindex(j);
-    hang_desire=i-fix(i/ds)*ds;%行为起点
-    lie_desire=ceil(i/ds);    %列为终点
-    %将行和列改成image矩阵中的位置
-    ox=ceil(hang_desire/ynumber);
-    oy=hang_desire-(ox-1)*ynumber;
-    dx=ceil(lie_desire/ynumber);
-    dy=lie_desire-(dx-1)*ynumber;
+Desireline= tril(ODmatrix)+tril(ODmatrix')-diag(diag(ODmatrix));
+[select_row,select_col]= find(Desireline>10);            %选择所画的期望线的数值范围
+maxOD= max(max(Desireline));                             %找出期望线的最大值
+figure                          
+for n=1:length(select_row)         
+    %将行和列改成xy矩阵中的位置
+    ox=ceil(select_row(n)/ynum);
+    oy=select_row(n)-(ox-1)*ynum;
+    dx=ceil(select_col(n)/ynum);
+    dy=select_col(n)-(dx-1)*ynum;
     hold on
-plot3([x3(1,ox),x3(1,dx)],[y3(oy,1),y3(dy,1)],[0,0],'LineWidth',20*desireline(i)/maxdesireline); 
+    plot([x(1,ox),x(1,dx)],[y(oy,1),y(dy,1)],'LineWidth',20*Desireline(select_row(n),select_col(n))/maxOD); 
 end
-view(2)
+
+
+
+
+
+
+
+    
 
 
 
